c1b676f05117227f17f3d650de335dfb30f70339
stefchrys <chrysinus@gmail.com>
2016-01-09 18:17:56 +0100

    correction d'orthographe, changement couleur backgroud, et indentation des balises

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<body>
-		<p>Cette ligne coorespond à mon premier commit</p>
+		<p>Cette ligne correspond à mon premier commit</p>
+		<p>C'est pas super super mais ce n'est qu'un exercice </p>
 	</body>
-
 </html>
