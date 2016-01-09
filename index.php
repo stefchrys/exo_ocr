efcef6f0f898a2355db17b0e7ec4331373c34fae
stefchrys <chrysinus@gmail.com>
2016-01-09 18:21:15 +0100

    ajout du doctype et du head, ainsi que la liaison au fichier css

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<!DOCTYPE html>
 <html>
+	<head>
+		<link rel="steelsheet" href="style.css" />
+		<title>Title of the document</title>
+	</head>
 	<body>
 		<p>Cette ligne correspond à mon premier commit</p>
 		<p>C'est pas super super mais ce n'est qu'un exercice </p>
